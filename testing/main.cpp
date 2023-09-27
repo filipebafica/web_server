e2dddd62dfa80fc3b0d8fa7913b28292fefb9860
Adriano Faustino de Oliveira <adriano.oliveira@arquivei.com.br>
2023-09-26 23:59:57 -0300

    Adiciona metodo que remove espaços em branco e inicia implementação do parsing de diretivas para quebrar elas em blocos.

--- testing/main.cpp
+++ testing/main.cpp
@@ -20,11 +20,11 @@ public:
         }
     }
 
-    void parse()
+    std::vector<std::vector<std::string>> parse()
     {
         _removeComments();
-        // remove white spaces
-        // get directives
+        _removeWhiteSpaces();
+        return _parseDirectives();
     }
 
     std::string getFileContent() const
@@ -85,8 +85,65 @@ private:
         }
     }
 
-    void _removeWhiteSpaces(std::string &fileContent)
+    void _removeWhiteSpaces()
     {
+        std::string result;
+        for (std::size_t i = 0; i < _fileContent.size(); ++i) {
+            if (!std::isspace(_fileContent[i])) {
+                result += _fileContent[i];
+            }
+        }
+        _fileContent = result;
+    }
+
+    std::vector<std::string> parseBlockDirectives(const std::string& serverBlock)
+    {
+        std::vector<std::string> blockDirectives;
+        std::vector<std::string> lines = splitString(serverBlock, '\n');
+        bool insideBlock = false;
+
+        for (const std::string& line : lines) {
+            if (line.find('{') != std::string::npos) {
+                insideBlock = true;
+            }
+            if (insideBlock) {
+                blockDirectives.push_back(line);
+            }
+            if (insideBlock && line.find('}') != std::string::npos) {
+                insideBlock = false;
+            }
+        }
+
+        return blockDirectives;
+    }
+
+    std::vector<std::string> splitString(const std::string& input, char delimiter) 
+    {
+        std::vector<std::string> tokens;
+        std::string token;
+        std::istringstream tokenStream(input);
+
+        while (std::getline(tokenStream, token, delimiter)) {
+            tokens.push_back(token);
+        }
+
+        return tokens;
+    }
+
+
+    std::vector<std::vector<std::string>> _parseDirectives()
+    {
+        std::vector<std::vector<std::string>> directives;
+        std::vector<std::string> serverBlocks = splitString(_fileContent, '}');
+
+        for (const std::string& serverBlock : serverBlocks) {
+            if (!serverBlock.empty()) {
+                std::vector<std::string> blockDirectives = parseBlockDirectives(serverBlock + '}');
+                directives.push_back(blockDirectives);
+            }
+        }
+
+        return directives;
     }
 
     std::string _filePath;
@@ -98,8 +155,15 @@ int main()
     std::string filePath("testing.conf");
 
     FileParser parser(filePath);
-    parser.parse();
-    std::cout << parser.getFileContent() << std::endl;
+    std::vector<std::vector<std::string>> parsedDirectives = parser.parse();
+    std::cout << parser.getFileContent() << std::endl << std::endl;
+
+    for (const auto& serverDirectives : parsedDirectives) {
+        for (const auto& blockDirective : serverDirectives) {
+            std::cout << "Block Directive:\n" << blockDirective << "\n";
+            std::cout << "--------------------\n";
+        }
+    }
 
     return (0);
 }
