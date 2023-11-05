8d3a22f9f6649519e06d3f7e3ce52bee3763af57
victor-ln <vlima-nu@student.42sp.org.br>
2023-11-05 12:31:12 -0300

    feat: :rocket: Updating server resources (.html, .php)

--- static/listDir.php
+++ static/listDir.php
@@ -1,14 +1,16 @@
 <?php
-// Pega a parte da URL após o nome do arquivo (excluindo a barra inicial)
-$requestedDirectory = ltrim($_SERVER['REQUEST_URI'], '/');
-$directoryPath = __DIR__ . '/' . $requestedDirectory;
 
-// Verifica se o caminho do diretório existe e é um diretório válido
+$requestedDirectory = $_SERVER['REQUEST_URI'];
+$lastSlashPosition = strrpos($requestedDirectory, '/');
+if ($lastSlashPosition !== false) {
+    $requestedDirectory = substr($requestedDirectory, 0, $lastSlashPosition);
+}
+
+$directoryPath = $_SERVER['DOCUMENT_ROOT'] . $requestedDirectory;
+
 if (is_dir($directoryPath)) {
-    // Abre o diretório
     $directory = opendir($directoryPath);
 
-    // Exibe uma lista de links para os arquivos no diretório
     echo '<h1>Conteúdo do Diretório: ' . $requestedDirectory . '</h1>';
     echo '<ul>';
     while (($file = readdir($directory)) !== false) {
@@ -19,10 +21,8 @@ if (is_dir($directoryPath)) {
     }
     echo '</ul>';
 
-    // Fecha o diretório
     closedir($directory);
 } else {
-    // Caso o diretório não exista, exibe uma mensagem de erro
     echo '<h1>Diretório não encontrado: ' . $requestedDirectory . '</h1>';
 }
 ?>
--- static/postFile.php
+++ static/postFile.php
@@ -1,18 +1,14 @@
 <?php
-$target_dir = "."; // Diretório onde o arquivo será armazenado
 
-if(isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] === UPLOAD_ERR_OK) {
-    // Verifique se o envio ocorreu sem erros
+print_r($_FILES);
+print_r($_SERVER);
 
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
+    $target_file = basename($_FILES["file"]["name"]);
 
-    // Verifique o tipo de arquivo (opcional)
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
     if($imageFileType === "txt") {
-        // O arquivo é do tipo TXT
-
-        // Movendo o arquivo para o diretório de destino
-        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
             echo "Arquivo enviado com sucesso.";
         } else {
             echo "Ocorreu um erro ao enviar o arquivo.";
